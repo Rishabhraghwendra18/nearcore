53cb4261dc69dfc9ab7604d338b54a3c7a320c15
Aleksey Kladov <aleksey.kladov@gmail.com>
2021-11-23 17:13:15 +0300

    docs: document consistent import style (#5290)
    Document whether we should do
    
    ```rust
    use near_store::{ColPeers, Store};
    use std::collections::HashMap;
    use std::net::SocketAddr;
    ```
    
    or
    
    ```rust
    use std::collections::HashMap;
    use std::net::SocketAddr;
    
    use near_store::{ColPeers, Store};
    ```
    
    This PR currently documents both suggested guidelines, and we'll use this PR to decide which one we pick.
    
    Decision procedure: if you don't care about the style, ignore this PR, and just note the final decision :) Otherwise, react with :+1: for proposal A or proposal B (do read the actual proposals in the changset). Feel free to leave a comment explaining your choice.
    
    If there's a clear consensus around one option, we'll just pick it. Otherwise, @bowenwang1996 makes the final call or delegates the decision to make the final call.

--- chain/network/src/peer/codec.rs
+++ chain/network/src/peer/codec.rs
@@ -109,7 +109,7 @@ fn peer_id_type_field_len(enum_var: u8) -> Option<usize> {
     }
 }
 
-pub(crate) fn is_forward_tx(bytes: &[u8]) -> Option<bool> {
+pub(crate) fn is_forward_transaction(bytes: &[u8]) -> Option<bool> {
     let peer_message_variant = *bytes.get(0)?;
 
     // PeerMessage::Routed variant == 13
@@ -152,7 +152,7 @@ pub(crate) fn is_forward_tx(bytes: &[u8]) -> Option<bool> {
 
 #[cfg(test)]
 mod test {
-    use crate::peer::codec::{is_forward_tx, Codec, NETWORK_MESSAGE_MAX_SIZE_BYTES};
+    use crate::peer::codec::{is_forward_transaction, Codec, NETWORK_MESSAGE_MAX_SIZE_BYTES};
     use crate::routing::edge::EdgeInfo;
     use crate::types::{Handshake, HandshakeFailureReason, HandshakeV2, PeerMessage, SyncData};
     use crate::PeerInfo;
@@ -259,7 +259,7 @@ mod test {
         schemas.for_each(|s| {
             let msg = create_tx_forward(s);
             let bytes = msg.try_to_vec().unwrap();
-            assert!(is_forward_tx(&bytes).unwrap());
+            assert!(is_forward_transaction(&bytes).unwrap());
         })
     }
 
